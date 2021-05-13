4efb5239962d14e63745c3996ec4297326e710f9
Leeong <gamezzbbzz@gmail.com.com>
2021-05-14 01:49:36 +0700

    Heroku config

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router'
 import * as jquery from 'jquery'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap'
 
 import './assets/styles.css'
